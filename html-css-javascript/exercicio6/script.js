4c1e363e248b96d9a10b966725615b57a8778d92
Daniel867227 <daniel.mdsc91@gmail.com>
2025-02-19 15:06:16 -0300

    finalizacao do exercicio6

--- html-css-javascript/exercicio6/script.js
+++ html-css-javascript/exercicio6/script.js
@@ -1,47 +1,29 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img =window.document.getElementById('imagem')
+    var data = new Date ()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} Horas`
 
-hora_agora = 8
-var corpo = window.document.getElementById('corpo')
-var titulo1= window.document.getElementById('titulo1')
-var manha =window.document.getElementById('manha')
-var p1=window.document.getElementById('p1')
-var tarde = window.document.getElementById('tarde')
-var p2 = window.document.getElementById('p2')
 
-if(hora_agora>=6 && hora_agora<=12){
+
+    if (hora>=0 && hora<12)
+    {
+
+        img.src='imagens/manha.jpg'
+        document.body.style.background='yellow'
+    }
+        else if(hora>=12 && hora<18){
+        
+        img.src='imagens/tarde.jpg'
+        document.body.style.background='brown'
+        }
+        else{
+
+        img.src='imagens/noite.jpg'
+        document.body.style.background='black'
+
+        }
+       
     
-    corpo.style.background='yellow'
-    titulo1.style.color ='white'
-    manha.style.visibility='visible'
-    p1.innerHTML= `Agora são ${hora_agora}`
-    
-
-
-}
-
-
-if (hora_agora>12 && hora_agora<=18){
-
-    corpo.style.background='brown'
-    titulo1.style.color ='white'
-    tarde.style.visibility='visible'
-    p2.innerHTML=`Agora são ${hora_agora}`
-
-    
-}
-
-if (hora_agora>18 && hora_agora<=23){
-    
-    corpo.style.background='black'
-    titulo1.style.color ='white'
-    
-}
-
-if(hora_agora>23 && hora_agora<=5){
-    
-    corpo.style.background='black'
-    titulo1.style.color ='white'
-
-}
-
-
-
+    }
