1e3c4540042e1f7ff9ad7c96c988cfdb80b3796b
Daniel867227 <daniel.mdsc91@gmail.com>
2025-02-21 00:20:37 -0300

    continuacao do exercicio7

--- html-css-javascript/exercicio7/script.js
+++ html-css-javascript/exercicio7/script.js
@@ -1,23 +1,73 @@
 
-
+var botao = window.document.getElementById('botao')
 botao.addEventListener('click', idade_e_foto)
 
-function idade_e_foto(){
-
+function idade_e_foto(){    
+    
     var ano_nasc = window.document.getElementById('ano_nasc')
     var ano = new Date ()
     var ano_atual = ano.getFullYear()
-    var idade = ano_atual - Number(ano_nasc.value)
-    var botao = window.document.getElementById('botao')
-    var res = window.document.getElementById('res')
-    var idade_num=Number(idade.valueOf)
-    res.innerHTML=`A idade da pessoa é: ${idade} anos`
-    var img = window.document.createElement('img')
-    // estou criando um elelemto. como se tivesse criando ele no html. mas crio no js somente em determinadas situacaoes
-    img.setAttribute('id','foto')
-    img.setAttribute('src', 'exercicio7.jpeg')
-    res.appendChild(img)
-    res.style.textAlign='center'
-}
 
+    if(ano_nasc.value.length ==0 || Number(ano_nasc.value)>ano_atual || Number(ano_nasc.value)<1900){
+
+        window.alert('[ERRO] Digite um valor válido')
+    }
+    
+    else{
+        
+        sex = window.document.getElementsByName('sex')
+       
+        if(sex[0].checked){
+
+                var idade = ano_atual - Number(ano_nasc.value)
+                
+                if(idade>=0 && idade<10){
+
+                    var res = window.document.getElementById('res')
+                    res.innerHTML=`A idade da pessoa é: ${idade} anos`
+                    var img = window.document.createElement('img')
+                    img.setAttribute('id','foto')
+                    img.setAttribute('src', 'exercicio7.jpeg')
+                    res.appendChild(img)
+                    img.style.borderRadius='50%'
+                    img.style.padding='20px'
+                    res.style.textAlign='center'
+
+                }
+
+                else if(idade>=10 && idade<16 ){
+
+                    var res = window.document.getElementById('res')
+                    res.innerHTML=`A idade da pessoa é: ${idade} anos`
+                    var img = window.document.createElement('img')
+                    img.setAttribute('id','foto')
+                    img.setAttribute('src', '../exercicio6/imagens/noite.jpg')
+                    res.appendChild(img)
+                    img.style.borderRadius='50%'
+                    img.style.padding='20px'
+                    res.style.textAlign='center'
+
+                
+
+                }
+
+        }
+
+        else {
+
+            var idade = ano_atual - Number(ano_nasc.value)    
+            var res = window.document.getElementById('res')
+            res.innerHTML=`A mulher tem a idade de ${idade} anos`
+
+
+
+        }
+
+
+    }
+
+     
+
+
+}
 
